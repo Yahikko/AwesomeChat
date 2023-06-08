cdbe45b867d7d4694d7ff6988cebda33eccf61b1
Sendzegakhara <sendzegakhara@mail.ru>
2023-06-08 14:07:25 +0300

    some code in MainActivity

--- app/src/main/java/com/example/awesomechat/MainActivity.java
+++ app/src/main/java/com/example/awesomechat/MainActivity.java
@@ -3,12 +3,84 @@ package com.example.awesomechat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView messageListView;
+    private AwesomeMessageAdapter adapter;
+    private ProgressBar progressBar;
+    private ImageButton sendImageButton;
+    private Button sendMessageButton;
+    private EditText messageEditText;
+
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = "Default User";
+        List<AwesomeMessage> awesomeMessages = new ArrayList<>();
+
+        progressBar = findViewById(R.id.progressBar);
+        messageListView = findViewById(R.id.messageListView);
+        sendImageButton = findViewById(R.id.sendPhotoButton);
+        sendMessageButton = findViewById(R.id.sendMessageButton);
+        messageEditText = findViewById(R.id.messageEditText);
+
+        adapter = new AwesomeMessageAdapter(this, R.layout.message_item, awesomeMessages);
+        messageListView.setAdapter(adapter);
+
+        progressBar.setVisibility(ProgressBar.INVISIBLE);
+
+        messageEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() > 0) {
+                    sendMessageButton.setEnabled(true);
+                } else {
+                    sendMessageButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        messageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(500)});
+
+        sendMessageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageEditText.setText("");
+            }
+        });
+
+        sendImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
