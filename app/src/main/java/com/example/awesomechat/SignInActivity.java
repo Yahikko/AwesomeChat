edf8a5eb2d2ec34dc93faefccde0093a10c436c5
Sendzegakhara <sendzegakhara@mail.ru>
2023-06-08 18:20:19 +0300

    authentication added

---
+++ app/src/main/java/com/example/awesomechat/SignInActivity.java
@@ -0,0 +1,147 @@
+package com.example.awesomechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private static final String TAG = "SignInActivity";
+
+    private FirebaseAuth auth;
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText nameEditText;
+    private EditText repeatPasswordEditText;
+    private TextView toggleLoginSingUpTextView;
+    private Button loginSignUpButton;
+
+    private boolean loginModeActive = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        repeatPasswordEditText = findViewById(R.id.repeatPasswordEditText);
+        nameEditText = findViewById(R.id.nameEditText);
+        toggleLoginSingUpTextView = findViewById(R.id.toggleLoginSingUpTextView);
+        loginSignUpButton = findViewById(R.id.loginSignUpButton);
+
+        auth = FirebaseAuth.getInstance();
+
+        loginSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginSignUpUser(emailEditText.getText().toString().trim(),
+                        passwordEditText.getText().toString().trim());
+            }
+        });
+
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+        }
+    }
+
+    private void loginSignUpUser(String email, String password) {
+
+        if (loginModeActive) {
+            if (passwordEditText.getText().toString().trim().length() < 7) {
+                Toast.makeText(this, "Passwords must be at least 7 characters",
+                        Toast.LENGTH_LONG).show();
+            } else if (emailEditText.getText().toString().trim().equals("")) {
+                Toast.makeText(this, "Please input your email",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    //updateUI(user);
+
+                                    startActivity(new Intent(SignInActivity.this,
+                                            MainActivity.class));
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure",
+                                            task.getException());
+                                    Toast.makeText(SignInActivity.this,
+                                            "Authentication failed",
+                                            Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+                            }
+                        });
+            }
+        } else {
+            if (!passwordEditText.getText().toString().trim().equals(
+                    repeatPasswordEditText.getText().toString().trim())) {
+                Toast.makeText(this, "Passwords don't match",
+                        Toast.LENGTH_LONG).show();
+            } else if (passwordEditText.getText().toString().trim().length() < 7) {
+                Toast.makeText(this, "Passwords must be at least 7 characters",
+                        Toast.LENGTH_LONG).show();
+            } else if (emailEditText.getText().toString().trim().equals("")) {
+                Toast.makeText(this, "Please input your email",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    //updateUI(user);
+
+                                    startActivity(new Intent(SignInActivity.this,
+                                            MainActivity.class));
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, ":createUserWithEmailFailure",
+                                            task.getException());
+                                    Toast.makeText(SignInActivity.this,
+                                            "Authentication failed",
+                                            Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+                            }
+                        });
+            }
+        }
+    }
+
+    public void toggleLoginMode(View view) {
+
+        if (loginModeActive) {
+            loginModeActive = false;
+            loginSignUpButton.setText("Sign Up");
+            toggleLoginSingUpTextView.setText("Or, Log In");
+            repeatPasswordEditText.setVisibility(View.VISIBLE);
+        } else {
+            loginModeActive = true;
+            loginSignUpButton.setText("Log In");
+            toggleLoginSingUpTextView.setText("Or, Sign Up");
+            repeatPasswordEditText.setVisibility(View.GONE);
+        }
+    }
+}
