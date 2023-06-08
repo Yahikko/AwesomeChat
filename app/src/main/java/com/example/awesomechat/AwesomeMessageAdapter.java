1cffadcacaaef4f7de86ed0f761abbb38c199136
Sendzegakhara <sendzegakhara@mail.ru>
2023-06-08 13:46:21 +0300

    activity_ and message_ layouts added; Adapter implemetned

---
+++ app/src/main/java/com/example/awesomechat/AwesomeMessageAdapter.java
@@ -0,0 +1,56 @@
+package com.example.awesomechat;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class AwesomeMessageAdapter extends ArrayAdapter<AwesomeMessage> {
+
+    public AwesomeMessageAdapter(@NonNull Context context, int resource,
+                                 List<AwesomeMessage> messages) {
+        super(context, resource);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().
+                    inflate(R.layout.message_item, parent, false);
+        }
+        ImageView photoImageView = convertView.findViewById(R.id.photoImageView);
+        TextView messageTextView = convertView.findViewById(R.id.messageTextView);
+        TextView nameTextView = convertView.findViewById(R.id.nameTextView);
+
+        AwesomeMessage message = getItem(position);
+
+        boolean isText = message.getImageUrl() == null;
+
+        if (isText) {
+            messageTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            messageTextView.setText(message.getText());
+        } else {
+            messageTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            Glide.with(photoImageView).load(message.getImageUrl())
+                    .load(photoImageView).into(photoImageView);
+        }
+
+        nameTextView.setText(message.getName());
+
+        return convertView;
+    }
+}
